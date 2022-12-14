25d0c70ac6911980ad7c754e5323b2179653f0c9
FabrizioMisseri <misserifabrizio@gmail.com>
2022-12-14 18:22:53 +0100

    one-shot

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    $data = [
+        'helloWorld' => 'HELLO WORLD',
+        'greetingArray' => [
+            'ciao',
+            'sono',
+            'Fabrizio',
+        ],
+    ];
+    return view('home', $data);
+})->name('home');
